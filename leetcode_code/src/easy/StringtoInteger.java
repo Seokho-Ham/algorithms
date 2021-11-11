6524f0e47dc0bde42878dfffd4e637be97f316a3
함석호 <kalejuice02@gmail.com>
2021-11-11 13:01:39 +0900

    11월 9일 Leetcode 1문제 풀이

--- leetcode_code/src/easy/StringtoInteger.java
+++ leetcode_code/src/easy/StringtoInteger.java
@@ -1,45 +1,30 @@
 package easy;
 
-import java.util.ArrayList;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class StringtoInteger {
     public int myAtoi(String s) {
-        boolean positive = true;
-        String answer = "";
+        s = s.trim();
+        if (s == null || s.isEmpty()) { return 0; }
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '-') {
-                positive = false;
-            } else if (Character.isDigit(s.charAt(i))) {
-                answer += Character.toString(s.charAt(i));
-            } else if (s.charAt(i) == ' ') {
-                continue;
-            } else {
-                break;
-            }
-        }
-        if (answer.length() < 1) {
-            return 0;
-        }
-        int result = 0;
-        try {
-            result = positive ? Integer.parseInt(answer) : Integer.parseInt(answer) * -1;
-        } catch (NumberFormatException e) {
-            if (positive) {
-                return Integer.MAX_VALUE;
-            } else {
-                return Integer.MIN_VALUE;
-            }
+        char[] chars = s.toCharArray();
+        int signum = 1, head = 0;
+        if (chars[head] == '+' || chars[head] == '-') { // treat sign
+            if (chars[head] == '-') { signum = -1; }
+            head++;
         }
 
-        return result;
+        long result = 0l;
+        long max = (long)Integer.MAX_VALUE;
+        long min = (long)Integer.MIN_VALUE;
+        while (head < chars.length && Character.isDigit(chars[head])) {
+            result = (result * 10) + (((int)chars[head++]-'0') * signum); // char '0' = int 48, in ascii
+            if (result > 0 && result > max) { result = max; break; }
+            if (result < 0 && result < min) { result = min; break; }
+        }
+        return (int)result;
     }
 }
 
-
-//수도코드
-//1. -, + 를 구분한다.
-//2. 문자열이 오면 끊는다.
-//3. 공백은 무시한다.
-//4. int의 범위가 넘어가면 최대값, 최소값으로 반환한다.
