e6af6c33ec44f1d15ac2a57a289bd7d9a1b8d689
함석호 <kalejuice02@gmail.com>
2022-05-18 00:10:45 +0900

    최신화

--- programmers/src/level2/OpenChat.java
+++ programmers/src/level2/OpenChat.java
@@ -12,24 +12,24 @@ public class OpenChat {
         List<String[]> actions = new ArrayList<>();
 
         for (String r : record) {
-            String[] action = r.split(" ");
+            String[] splitRecord = r.split(" ");
 
-            if (action[0].equals("Enter")) {
-                userList.put(action[1], action[2]);
-                actions.add(new String[]{action[0], action[1]});
-            } else if (action[0].equals("Change")) {
-                userList.put(action[1], action[2]);
+            if (splitRecord[0].equals("Enter")) {
+                userList.put(splitRecord[1], splitRecord[2]);
+                actions.add(new String[]{splitRecord[0], splitRecord[1]});
+            } else if (splitRecord[0].equals("Change")) {
+                userList.put(splitRecord[1], splitRecord[2]);
             } else {
-                actions.add(new String[]{action[0], action[1]});
+                actions.add(new String[]{splitRecord[0], splitRecord[1]});
             }
         }
 
         String[] answer = new String[actions.size()];
 
         for (int i = 0; i < actions.size(); i++) {
-            String act = actions.get(i)[0];
+            String action = actions.get(i)[0];
             String id = actions.get(i)[1];
-            if (act.equals("Enter")) {
+            if (action.equals("Enter")) {
                 answer[i] = userList.get(id) + "님이 들어왔습니다.";
             } else {
                 answer[i] = userList.get(id) + "님이 나갔습니다.";
@@ -38,5 +38,15 @@ public class OpenChat {
 
         return answer;
     }
-
 }
+
+key: id, value: name으로 저장하는 Map객체 생성
+입장, 퇴장을 기록할 List객체 생성 (동작과 id를 배열 형태로 저장)
+
+1. 반복문을 돌면서 기록을 저장
+- Enter일 경우 Map에 id와 name을 저장 & List에 id와 동작 저장
+- Change일 경우 Map에 저장된 name을 변경
+- Leave일 경우 List에 id와 동작 저장
+
+2. 배열에 결과값을 저장
+- actions List에서 동작과 id값 꺼내온 뒤, Map에서 해당하는 name을 꺼내와서 배열에 저장
